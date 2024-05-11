82f3d90732dd7a4076bf60aac900f314f5940cc4
Ruben Virtic <rdvirtic@gmail.com>
2024-05-11 16:27:05 +0200

    LinkedHashmapExample done

--- src/main/java/_03_04/end/LinkedHashmapExample.java
+++ src/main/java/_03_04/end/LinkedHashmapExample.java
@@ -5,14 +5,21 @@ import java.util.LinkedHashMap;
 public class LinkedHashmapExample {
 
     public static void main(String[] args) {
-        LinkedHashMap<String, Integer> basket = new LinkedHashMap<>(10, 0.75f, true);
+        LinkedHashMap<String, Integer> basket1 = new LinkedHashMap<>();
+        LinkedHashMap<String, Integer> basket2 = new LinkedHashMap<>(10,0.75f, true);
 
-        basket.put("apple", 2);
-        basket.put("orange", 1);
-        basket.put("banana", 3);
+        System.out.println("First basket (normal LinkedHashMap):");
+        basket1.put("apple", 2);
+        basket1.put("orange", 1);
+        basket1.put("banana", 3);
+        basket1.forEach((key, value) -> System.out.println(key + ": " + value)); // shows all items in the order that they were INSERTED in
 
-        basket.get("apple");
-
-        basket.forEach((key, value) -> System.out.println(key + ": " + value));
+        System.out.println("\nSecond basket (with arguments in the constructor):");
+        basket2.put("apple", 2);
+        basket2.put("orange", 1);
+        basket2.put("banana", 3);
+        basket2.get("apple");
+        basket2.forEach((key, value) -> System.out.println(key + ": " + value)); // shows all items in the order that they were last ACCESSED in
+        
     }
 }
